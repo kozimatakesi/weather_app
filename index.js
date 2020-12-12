450a42e19c70cfefb73e933e42d5698b49896b5b
riverbook708 <riverbook708@gmail.com>
2020-12-12 15:02:47 +0900

    change variable name

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-global.fetch = require('node-fetch'); //fetchをnodeで使用するためのもの
+global.fetch = require('node-fetch');
 
 console.log('こんにちは');
 async function callApi(){
-  const res = await fetch("http://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=9a4d371b6fc452d3edd2f79b142c8c18");
+  const res = await fetch("http://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=9a4d371b6fc452d3edd2f79b142c8c18&lang=ja&units=metric");
   //console.log(res);
-  const users = await res.json();
-  const place = users.name;
-  console.log(place + "は" + users.weather[0].main + "です");
+  const results = await res.json();
+  //console.log(results);
+  const place = results.name;
+  console.log("現在の" + place + "は" + results.weather[0].description + "です");
 }
 
 callApi();
